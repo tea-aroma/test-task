aa78a1cfb207986a61f42c15f0afd9cc5efa0480
tea-aroma <kovalyovictor71@gmail.com>
2025-05-17 02:24:57 +0500

    feat!: Add change logic in 'Repository'.

--- app/Standards/Repositories/Abstracts/Repository.php
+++ app/Standards/Repositories/Abstracts/Repository.php
@@ -4,6 +4,7 @@ namespace App\Standards\Repositories\Abstracts;
 
 
 use App\Standards\Data\Abstracts\Data;
+use App\Standards\Repositories\Interfaces\ReadableInterface;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Provides the base abstract logic for managing database tables.
  */
-class Repository
+class Repository implements ReadableInterface
 {
     /**
      * The model class or instance.
@@ -21,11 +22,11 @@ class Repository
     protected string|Model $model;
 
     /**
-     * @param Model $model
+     * @param Model|null $model
      */
-    public function __construct(Model $model)
+    public function __construct(?Model $model = null)
     {
-        $this->model = isset($this->model) ? new $model() : $model;
+        $this->model = isset($this->model) ? new $this->model() : $model ?? new $this->model();
     }
 
     /**
@@ -36,6 +37,18 @@ class Repository
     protected function getKeyName(): string
     {
         return $this->model->getKeyName();
+    }
+
+    /**
+     * Determines whether a record by the specified id exists.
+     *
+     * @param int $id
+     *
+     * @return bool
+     */
+    public function exists(int $id): bool
+    {
+        return static::find($id) !== null;
     }
 
     /**
@@ -59,57 +72,6 @@ class Repository
      */
     public function find(int $id): ?Model
     {
-        return $this->model::query()->find($id);
-    }
-
-    /**
-     * Creates a new record by the specified attributes.
-     *
-     * @param Data $attributes
-     *
-     * @return Model
-     */
-    public function create(Data $attributes): Model
-    {
-        return $this->model::query()->create($attributes->toArray());
-    }
-
-    /**
-     * Updates a record by the specified attributes.
-     *
-     * @param Data     $attributes
-     * @param int|null $id
-     *
-     * @return int
-     */
-    public function update(Data $attributes, int $id = null): int
-    {
-        $id = $attributes->id ?? $id;
-
-        return $this->model::query()->where($this->getKeyName(), '=', $id)->update($attributes->toArray());
-    }
-
-    /**
-     * Deletes a record by the specified id.
-     *
-     * @param int $id
-     *
-     * @return bool
-     */
-    public function delete(int $id): bool
-    {
-        return $this->model::query()->where($this->getKeyName(), '=', $id)->delete();
-    }
-
-    /**
-     * Determines whether a record by the specified id exists.
-     *
-     * @param int $id
-     *
-     * @return bool
-     */
-    public function exists(int $id): bool
-    {
-        return static::find($id) !== null;
+        return $this->model->newQuery()->find($id);
     }
 }
