ef1d5f789f54cda0aade32ed477a236f64c5fe5d
tea-aroma <kovalyovictor71@gmail.com>
2025-05-17 18:24:10 +0500

    feat: Add the 'currencies' log channel and enum for channels.

---
+++ app/Standards/Logs/Enums/LogChannelsEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Standards\Logs\Enums;
+
+
+/**
+ * The enum contains all possible log channels.
+ */
+enum LogChannelsEnum: string
+{
+    case CURRENCIES = 'currencies';
+}
--- config/logging.php
+++ config/logging.php
@@ -58,6 +58,13 @@ return [
             'ignore_exceptions' => false,
         ],
 
+        'currencies' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/currencies.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true,
+        ],
+
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
