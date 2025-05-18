9a4a5acd887591ab5df7102bcc7989fab63ed769
tea-aroma <kovalyovictor71@gmail.com>
2025-05-18 17:48:46 +0500

    refactor: Add redis to docker-compose; Update the cron logic.

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-\Illuminate\Support\Facades\Schedule::command('currencies:update')
+\Illuminate\Support\Facades\Schedule::command('currencies:update')->cron(str_replace('_', ' ', env('CURRENCIES_UPDATE_INTERVAL', '0_*_*_*_*')))
     ->onSuccess(function ()
     {
         \Illuminate\Support\Facades\Log::channel(LogChannelsEnum::CURRENCIES->value)->info('Successfully updated currencies.');
