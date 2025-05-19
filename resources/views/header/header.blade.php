a74389c9ff8498b34659294970e3e97e4caca0d2
tea-aroma <kovalyovictor71@gmail.com>
2025-05-19 15:59:47 +0500

    fear: Add menu component.

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,8 +1,26 @@
 <header class="header header--style-default">
 
-    <svg width="15px" height="11px" class="header__icon">
-        <use xlink:href="{{ asset('assets/icons/sprite.svg') }}#currency-outline"></use>
-    </svg>
+    <div class="header__column">
+        <svg width="15px" height="11px" class="header__icon">
+            <use xlink:href="{{ asset('assets/icons/sprite.svg') }}#currency-outline"></use>
+        </svg>
 
-    <h1 class="header__heading">Currencies</h1>
+        <h1 class="header__heading">Currencies</h1>
+    </div>
+
+    <ul class="menu menu--style-default menu--type-default">
+
+        <li class="menu__item {{ \Illuminate\Support\Facades\Route::currentRouteName() !== 'home' ?: 'active' }}">
+            <a href="{{ route('home') }}" class="menu__link">Home Page</a>
+        </li>
+
+        <li class="menu__item {{ \Illuminate\Support\Facades\Route::currentRouteName() !== 'settings.system' ?: 'active' }}">
+            <a href="{{ route('settings.system') }}" class="menu__link">System Settings</a>
+        </li>
+
+        <li class="menu__item {{ \Illuminate\Support\Facades\Route::currentRouteName() !== 'settings.currencies' ?: 'active' }}">
+            <a href="{{ route('settings.currencies') }}" class="menu__link">System Currencies</a>
+        </li>
+
+    </ul>
 </header>
