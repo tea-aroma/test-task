ac74dfb6809a4709dd36faeb7dc8832aa4c11483
tea-aroma <kovalyovictor71@gmail.com>
2025-05-20 11:27:58 +0500

    refactor: Add the check for 'settings' table.

--- routes/console.php
+++ routes/console.php
@@ -3,18 +3,23 @@
 use App\Standards\Logs\Enums\LogChannelsEnum;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-\Illuminate\Support\Facades\Schedule::command('currencies:update')
-    ->cron(str_replace('_', ' ', \App\Standards\Settings\SettingsEnum::CRON_AUTOUPDATE_INTERVAL->getValue()))
-    ->onSuccess(function ()
-    {
-        \Illuminate\Support\Facades\Log::channel(LogChannelsEnum::CURRENCIES->value)->info('Successfully updated currencies.');
-    })
-    ->onFailure(function ()
-    {
-        \Illuminate\Support\Facades\Log::channel(LogChannelsEnum::CURRENCIES->value)->error('Failed to update currencies.');
-    });
+if (!Schema::hasTable('settings'))
+{
+    \Illuminate\Support\Facades\Schedule::command('currencies:update')
+        ->cron(str_replace('_', ' ', \App\Standards\Settings\SettingsEnum::CRON_AUTOUPDATE_INTERVAL->getValue()))
+        ->onSuccess(function ()
+        {
+            \Illuminate\Support\Facades\Log::channel(LogChannelsEnum::CURRENCIES->value)->info('Successfully updated currencies.');
+        })
+        ->onFailure(function ()
+        {
+            \Illuminate\Support\Facades\Log::channel(LogChannelsEnum::CURRENCIES->value)->error('Failed to update currencies.');
+        });
+}
